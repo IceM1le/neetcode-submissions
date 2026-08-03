40c3dbbcc89ed1e9c4c2d94d011d9f7c0c17d5e2
Danil <74837183+IceM1le@users.noreply.github.com>
2026-08-04 00:38:13 +0300

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count_char = dict()
+        n = len(s1)
+        for i in range(n):
+            count_char[s1[i]] = count_char.get(s1[i], 0) + 1
+        left = 0
+        counts = dict()
+        for right in range(len(s2)):
+            if s2[right] in count_char:
+                if not counts: left = right
+                counts[s2[right]] = counts.get(s2[right], 0) + 1
+                if right - left + 1 == n:
+                    if counts == count_char:
+                        return True
+                    else:
+                        counts[s2[left]] -= 1
+                        left += 1                        
+            else:
+                counts = dict()                
+        return False
