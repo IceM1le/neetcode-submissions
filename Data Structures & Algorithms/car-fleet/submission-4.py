28201a2e6c6621bc72265d9dec1b2d2fc0bbcb6d
Danil <74837183+IceM1le@users.noreply.github.com>
2026-08-05 21:23:37 +0300

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        dict_speed = {}
+        for i, pos in enumerate(position):
+            dict_speed[pos] = speed[i]
+        position = sorted(position)
+        stack = []
+        for i, pos in enumerate(position):
+            speed[i] = dict_speed[pos]
+        count = 0
+        for i in range(len(position)):            
+            steps = (target - position[i]) / speed[i]
+            while stack and stack[-1] <= steps:                
+                stack.pop() 
+            stack.append(steps)        
+        return len(stack)
